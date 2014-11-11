5be80dd0f38b41f5f5ad33b2667f8ad631c1072b
sunteya <sunteya@gmail.com>
2014-11-11 23:11:13 +0800

    use "Syntax Manager" and "colorcoder" improve language settings

--- setup_path_env.py
+++ setup_path_env.py
@@ -3,7 +3,7 @@ import os
 print('OLD PATH = ' + os.environ['PATH'])
 
 HOME = os.path.expanduser("~")
-paths = [ HOME + "/.rbenv/shims", "/usr/local/bin" ]
+paths = [ HOME + "/.rbenv/shims", HOME + "/.ndenv/shims", "/usr/local/bin" ]
 path_string = ":".join(paths)
 if os.environ['PATH'].find(path_string) == -1:
     os.environ['PATH'] = path_string + ":" + os.environ['PATH']
